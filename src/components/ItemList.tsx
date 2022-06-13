66a2857c620f9890b0242e940034cf2f6ff15f04
pranit1 <pspranits10494@gmail.com>
2022-06-14 02:45:14 +0530

    wiring up input and adding task list

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
-import { useAppSelector } from '../app/hooks'
-//import { addTodo,editTodo } from "../features/todoSlice";
+import { useAppSelector, useAppDispatch } from '../app/hooks'
+import { addTodo,editTodo } from "../features/todoSlice";
 import { selectItems } from "../features/todoSlice";
 const TodoApp  = styled.div`
   display: grid;
@@ -39,8 +39,7 @@ const Sidebar = styled.aside`
 `
 
 const Main = styled.main`
-  
-grid-area: content;
+  grid-area: content;
   display:flex;
   flex-direction:column;
   justify-content:flex-start;
@@ -69,20 +68,75 @@ const AddButton = styled.button`
   border:#0078d7;
   color:#ffffff;
 `
-
+const TaskContainer = styled.div`
+  background-color:#f3f2f1;
+  display:flex;
+  flex-direction:column;
+  width:790px;
+  justify-content:flex-start;
+  align-items:center;
+  padding:0 1em 0 1em;
+  h3 {
+    padding-top:1em;
+    margin:0 0;
+   align-self:flex-start;
+  }
+`
+const TaskList = styled.ul`
+width:100%;
+padding:1em;
+list-style:none;
+`
+const Task = styled.li`
+display:flex;
+justify-content:flex-start;
+background-color:lightblue;
+border-bottom:1px solid lightgrey;
+padding:0 5px 0 5px;
+p {
+  padding:0 1em 0 1em;
+}
+input['checkbox'] {
+  border-radius: 50%;
+}
+`
+const ItemCheckbox = styled.input.attrs({
+  type: "checkbox",
+})`
+height:1.3em;
+width:1.3em;
+align-self:center;
+`;
 const ItemList: React.FC = () => {
   const items = useAppSelector(selectItems);
-  console.log(items)
+  const dispatch = useAppDispatch()
+  const [todo, setTodo] = useState<string>("");
   return (
    <TodoApp>
     <Header><h1>To Do</h1></Header>
     <Sidebar/>
     <Main>
       <InputContainer>
-        <Input placeholder="Add Todo.."/>
-        <AddButton>Add Todo</AddButton>
-      </InputContainer>
-      
+          <Input placeholder="Add Todo.." 
+            type="text"
+            onChange={(e) => setTodo(e.target.value)}
+            onKeyUp={(e) => {if(e.key === 'Enter') dispatch(addTodo(todo))}}
+            />
+          <AddButton onClick={() => dispatch(addTodo(todo))}>Add Todo</AddButton>
+      </InputContainer> 
+      <TaskContainer>
+        <h3>
+          Tasks
+        </h3>
+        <TaskList>
+          {items.map((item) => {
+            return <Task key={item.id}>
+             <ItemCheckbox/> 
+              <p>{item.text}</p>
+            </Task>
+          })}
+        </TaskList>
+      </TaskContainer>
     </Main>
     <Nav/>
    </TodoApp>
