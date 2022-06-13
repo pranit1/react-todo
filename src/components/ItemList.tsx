baa677621d9e72909f1284a116e5f26a7d3fccf5
pranit1 <pspranits10494@gmail.com>
2022-06-13 23:14:53 +0530

    refactoring layout and styled components

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -17,29 +17,57 @@ const TodoApp  = styled.div`
   grid-template-rows: 50px 1fr 0px;
 `;
 const Header = styled.header`
-grid-area: header;
-background-color:#0078d7;
-display:flex;
-justify-content:center;
-align-items:center;
-h1 {
-  font-size:20px;
-  line-height: 100%;
-  color:#ffffff;
-}
+  grid-area: header;
+  background-color:#0078d7;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  h1 {
+    font-size:20px;
+    line-height: 100%;
+    color:#ffffff;
+  }
 `
 
 const Nav = styled.nav`
-grid-area: nav;
-background-color:#f3f2f1;
+  grid-area: nav;
+  background-color:#f3f2f1;
 `
 const Sidebar = styled.aside`
-grid-area: sidebar;
-background-color:#f3f2f1;
+  grid-area: sidebar;
+  background-color:#f3f2f1;
 `
 
 const Main = styled.main`
+  
 grid-area: content;
+  display:flex;
+  flex-direction:column;
+  justify-content:flex-start;
+  align-items:center;
+  gap:1em;
+  padding: 30px 20px 0px 20px;
+`
+const InputContainer = styled.div`
+  background-color:#f3f2f1;
+  display:flex;
+  height:40px;
+  width:790px;
+  place-content:center;
+  padding:1em;
+  gap:1em;
+`
+const Input = styled.input `
+  height:70%;
+  width:80%;
+  font-size:large;
+`
+const AddButton = styled.button`
+  height:80%;
+  width:10%;
+  background-color:#0078d7;
+  border:#0078d7;
+  color:#ffffff;
 `
 
 const ItemList: React.FC = () => {
@@ -50,6 +78,10 @@ const ItemList: React.FC = () => {
     <Header><h1>To Do</h1></Header>
     <Sidebar/>
     <Main>
+      <InputContainer>
+        <Input placeholder="Add Todo.."/>
+        <AddButton>Add Todo</AddButton>
+      </InputContainer>
       
     </Main>
     <Nav/>
